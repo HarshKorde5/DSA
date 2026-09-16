553f83f13b66d0309d51c872fc95ea296ef98313
HarshKorde5 <harshkorde05@gmail.com>
2026-09-16 20:10:15 +0530

    Delegated getGraph and printGraph separately in Utils.java

---
+++ Graph/Utils.java
@@ -0,0 +1,52 @@
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Utils {
+    private static void accept(List<List<Integer>> graph) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number of nodes in graph they'll be numbered from 1 to n :: ");
+        int n = sc.nextInt();
+        System.out.println("Enter the number of edges in graph :: ");
+        int m = sc.nextInt();
+
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        System.out.println(
+                "Enter the edges int the format (u,v), NOTE : Nodes are numbered starting with 1 upto " + n + " ::");
+        for (int i = 0; i < m; i++) {
+            System.out.print("Enter u :: ");
+            int u = sc.nextInt();
+            System.out.print("Enter v :: ");
+            int v = sc.nextInt();
+
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
+
+        sc.close();
+    }
+
+    public static void getGraph(List<List<Integer>> graph) {
+        Utils.accept(graph);
+    }
+
+    private static void display(List<List<Integer>> graph) {
+        System.out.println("Graph details :: ");
+        for (int i = 1; i < graph.size(); i++) {
+            for (int j = 0; j < graph.get(i).size(); j++) {
+                System.out.println(i + "," + graph.get(i).get(j));
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void printGraph(List<List<Integer>> graph) {
+        Utils.display(graph);
+    }
+}
