5d893a559e5ed35750780a37bfa6903963768393
HarshKorde5 <harshkorde05@gmail.com>
2026-09-16 20:10:33 +0530

    delegation change

--- Graph/BFS.java
+++ Graph/BFS.java
@@ -2,51 +2,21 @@ import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Queue;
-import java.util.Scanner;
 
 public class BFS {
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter the number of nodes in graph they'll be numbered from 0 to n-1 :: ");
-        int n = sc.nextInt();
-        System.out.println("Enter the number of edges in graph :: ");
-        int m = sc.nextInt();
-
 
         List<List<Integer>> graph = new ArrayList<>();
-        for(int i = 0; i < n; i++){
-            graph.add(new ArrayList<>());
-        }
-
-        System.out.println("Enter the edges int the format (u,v), NOTE : Nodes are numbered starting with 0 upto "+(n-1)+" ::");
-        for(int i = 0; i < m; i++){
-            System.out.print("Enter u :: ");
-            int u = sc.nextInt();
-            System.out.print("Enter v :: ");
-            int v = sc.nextInt();
-
-            graph.get(u).add(v);
-            graph.get(v).add(u);
-        }
-
-        System.out.println("Graph details :: ");
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < graph.get(i).size(); j++){
-                System.out.println(i+","+graph.get(i).get(j));
-            }
-            System.out.println();
-        }
+        Utils.getGraph(graph);
+        Utils.printGraph(graph);
 
         System.out.println("BFS Traversal of graph is : ");
         boolean[] visited = new boolean[graph.size()];
-        for(int i = 0; i < n; i++){
+        for(int i = 1; i < graph.size(); i++){
             if(!visited[i]){
                 BFS.bfs(i, graph, visited);
             }
         }
-
-        sc.close();
     }
 
     static void bfs(int start, List<List<Integer>> graph, boolean[] visited){
--- Graph/DFS.java
+++ Graph/DFS.java
@@ -1,52 +1,24 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 import java.util.Stack;
 
 public class DFS {
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter the number of nodes in graph they'll be numbered from 0 to n-1 :: ");
-        int n = sc.nextInt();
-        System.out.println("Enter the number of edges in graph :: ");
-        int m = sc.nextInt();
-
 
         List<List<Integer>> graph = new ArrayList<>();
-        for(int i = 0; i < n; i++){
-            graph.add(new ArrayList<>());
-        }
+        Utils.getGraph(graph);
+        Utils.printGraph(graph);
 
-        System.out.println("Enter the edges int the format (u,v), NOTE : Nodes are numbered starting with 0 upto "+(n-1)+" ::");
-        for(int i = 0; i < m; i++){
-            System.out.print("Enter u :: ");
-            int u = sc.nextInt();
-            System.out.print("Enter v :: ");
-            int v = sc.nextInt();
-
-            graph.get(u).add(v);
-            graph.get(v).add(u);
-        }
-
-        System.out.println("Graph details :: ");
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < graph.get(i).size(); j++){
-                System.out.println(i+","+graph.get(i).get(j));
-            }
-            System.out.println();
-        }
 
         System.out.println("DFS Traversal of graph is : ");
         boolean[] visited = new boolean[graph.size()];
 
-        for(int i = 0; i < n; i++){
+        for(int i = 1; i < graph.size(); i++){
             if(!visited[i]){
                 DFS.dfs_rec(i, graph, visited);
             }
         }
 
-        sc.close();
     }
 
     static void dfs(int start, List<List<Integer>> graph, boolean[] visited){
